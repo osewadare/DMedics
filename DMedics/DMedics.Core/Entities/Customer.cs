d3fb4eadb8c09b22c1c4b69a4454257bd224a242
Chinonso Uzoamaka Mfon-Eddie <100494080+Nonnyeddie@users.noreply.github.com>
2022-03-07 12:28:34 +0000

    the entities are created

---
+++ DMedics/DMedics.Core/Entities/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMedics.Core.Entities
+{
+    public class Customer
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string PostCode { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string ReferralSource { get; set; }
+
+    }
+}
