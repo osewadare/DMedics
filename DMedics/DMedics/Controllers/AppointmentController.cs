8eb99c7a9d2cd186accb7e2c66462cc5c1816016
dareosewa <osewa.dare@gmail.com>
2022-03-13 11:46:37 +0000

    Create AppointmentController.cs

---
+++ DMedics/DMedics/Controllers/AppointmentController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DMedics.Services.APIModels;
+using DMedics.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DMedics.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentController : Controller
+    {
+
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        [HttpGet]
+        [Route("GetCreatedAppointmentDates")]
+        public IActionResult GetCreatedAppointmentDates()
+        {
+            var response = _appointmentService.GetCreatedAppointmentDates();
+            return Ok(response);
+
+        }
+
+
+        [HttpGet]
+        [Route("GetAvailableAppointmentTypes")]
+        public IActionResult GetAvailableAppointmentTypes()
+        {
+            var response = _appointmentService.GetCreatedAppointmentDates();
+            return Ok(response);
+        }
+
+
+        [HttpGet]
+        [Route("BookAppointment")]
+        public IActionResult BookAppointment(AppointmentRequestModel requestModel)
+        {
+            var response = _appointmentService.BookAppointment(requestModel);
+            return Ok(response);
+        }
+    }
+}
