16d972978da0d48673b2501188e588ab5c46d870
dareosewa <osewa.dare@gmail.com>
2022-04-13 17:42:28 +0100

    Added Unit Testing packages

--- DMedics/DMedics.Test/MainTest.cs
+++ DMedics/DMedics.Test/MainTest.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using Xunit;
+using Moq;
+using DMedics.Repository.Repository;
+using DMedics.Domain.Entities;
 
 namespace DMedics.Test
 {
-    public class Class1
+    public class MainTest
     {
+        private Mock<IBaseRepository<Appointment>> mockInvoiceRepo = new Mock<IBaseRepository<Appointment>>();
+
+        [Fact]
+        public void TestCase1()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+
     }
 }
