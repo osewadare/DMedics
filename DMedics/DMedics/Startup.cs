e780dbd448035ea0150742e605e789e59f7c7c54
dareosewa <osewa.dare@gmail.com>
2022-04-13 16:29:20 +0100

    Updated Readme with project description

--- DMedics/DMedics/Startup.cs
+++ DMedics/DMedics/Startup.cs
@@ -69,11 +69,13 @@ namespace DMedics
             app.UseHangfireDashboard();
 
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DMedics v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DMedics v1"));
+                
             }
 
             app.UseHttpsRedirection();
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# DMedics
+DMedics API Project
+
+DMedics provides medicals for drivers looking to obtain a medical certificate.
+This API Project exposes endpoints consumed by the DMedics.Web React Project 
+
+
+
